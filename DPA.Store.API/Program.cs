351282d09e2f8936bc04b9ba666e6296e053235d
ecuadrosg36 <ecuadrosg36@gmail.com>
2023-09-23 20:37:03 -0500

    great

--- DPA.Store.API/Program.cs
+++ DPA.Store.API/Program.cs
@@ -1,4 +1,6 @@
+using DPA.Store.DOMAIN.Core.Interfaces;
 using DPA.Store.DOMAIN.Infrastructure.Data;
+using DPA.Store.DOMAIN.Infrastructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +8,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var _config = builder.Configuration;
 var _cnx = _config.GetConnectionString("DevConnection");
+
 builder
     .Services
     .AddDbContext<StoreDbContext>(options =>
     {
         options.UseSqlServer(_cnx);
     });
+
+builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
